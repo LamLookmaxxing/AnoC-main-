3f0936ba9aa409c98e61f834d295d343b0ec3db7
LamLookmaxxing <tahuuphuclam3@gmail.com>
2024-10-26 23:09:57 +0700

    first commit

--- AnoC/settings.py
+++ AnoC/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-$uuf7y#5+pe0a$%ep5fs!zjd26y3tzzt-v11s*vki%_f@2a)%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
